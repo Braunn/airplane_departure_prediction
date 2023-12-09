bf939a77bbb157b9df629320b8213aadb064606b
Chris Wozny <1447143+cwozny@users.noreply.github.com>
2023-12-09 15:23:44 -0500

    command-line arguments, partition-awareness, and filtering

--- runRDD.py
+++ runRDD.py
@@ -1,48 +1,66 @@
+import argparse
+
 from pyspark import SparkContext
 from datetime import datetime
 
-N = 10
+if __name__ == "__main__":
 
-sc = SparkContext(appName='Airplane Departure Prediction')
+    parser = argparse.ArgumentParser(description = 'Airplane Departure Prediction',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--master',default="local[30]",help="Spark Master")
+    parser.add_argument('--N',type=int,default=30,help="Number of partitions to be used in RDDs containing departure and/or weather data.")
+    args = parser.parse_args()
 
-# Read in the airport departure statistics from CSV in to an RDD and split by commas
-departureRDD = sc.textFile("concatenated_data.csv").map(lambda x: x.split(","))
+    sc = SparkContext(args.master, 'Airplane Departure Prediction')
+    sc.setLogLevel('warn')
 
-# Strip the RDD down to the columns we care about: (origin airport, (scheduled departure date, scheduled departure time, weather delay in minutes))
-departureRDD = departureRDD.map(lambda x: (x[17], (x[1], x[5], float(x[13]))) ).repartition(N)
+    print("Command line arguments:")
+    print("--master",args.master)
+    print("--N",args.N)
 
-# Combine the date and time in to one string
-departureRDD = departureRDD.mapValues(lambda x: (x[0] + " " + x[1] + ":01", x[2]) )
+    # Read in the airport departure statistics from CSV in to an RDD and split by commas
+    departureRDD = sc.textFile("concatenated_data.csv").map(lambda x: x.split(","))
 
-# Convert that date/time string to a datetime object
-departureRDD = departureRDD.mapValues(lambda x: (datetime.strptime(x[0], "%m/%d/%Y %H:%M:%S"), x[1]) )
+    # Strip the RDD down to the columns we care about: (origin airport, (scheduled departure date, scheduled departure time, weather delay in minutes))
+    departureRDD = departureRDD.map(lambda x: (x[17], (x[1], x[5], float(x[13]))) ).repartition(args.N)
 
-# Cache it
-departureRDD = departureRDD.cache()
+    # Combine the date and time in to one string
+    departureRDD = departureRDD.mapValues(lambda x: (x[0] + " " + x[1] + ":01", x[2]) )
 
-# Debug print out a few of them
-print("departureRDD.count() =",departureRDD.count(),"\n")
-print("departureRDD.takeSample(False, 5) =\n",departureRDD.takeSample(False, 5),"\n")
+    # Convert that date/time string to a datetime object
+    departureRDD = departureRDD.mapValues(lambda x: (datetime.strptime(x[0], "%m/%d/%Y %H:%M:%S"), x[1]) )
 
-# Read in the weather data from CSV in to an RDD and split by commas
-weatherRDD = sc.textFile("cleaned_weather_data.csv").map(lambda x: x.split(","))
+    # Cache it
+    departureRDD = departureRDD.cache()
 
-# Strip the RDD down to the columns we care about: (AirportCode,(Type,Severity,StartTime(UTC),EndTime(UTC),Precipitation(in)))
-weatherRDD = weatherRDD.map(lambda x: (x[7],(x[1],x[2],x[3],x[4],float(x[5])))).repartition(N)
+    # Debug print out a few of them
+    print("departureRDD.count() =",departureRDD.count(),"\n")
+    print("departureRDD.takeSample(False, 5) =\n",departureRDD.takeSample(False, 5),"\n")
 
-# Convert that date/time string to a datetime object
-weatherRDD = weatherRDD.mapValues(lambda x: (x[0], x[1], datetime.strptime(x[2], "%Y-%m-%d %H:%M:%S"), datetime.strptime(x[3], "%Y-%m-%d %H:%M:%S"), x[4]) )
+    # Read in the weather data from CSV in to an RDD and split by commas
+    weatherRDD = sc.textFile("cleaned_weather_data.csv").map(lambda x: x.split(","))
 
-# Cache it
-weatherRDD = weatherRDD.cache()
+    # Strip the RDD down to the columns we care about: (AirportCode,(Type,Severity,StartTime(UTC),EndTime(UTC),Precipitation(in)))
+    weatherRDD = weatherRDD.map(lambda x: (x[7],(x[1],x[2],x[3],x[4],float(x[5])))).repartition(args.N)
 
-# Debug print out a few of them
-print("weatherRDD.count() =",weatherRDD.count(),"\n")
-print("weatherRDD.takeSample(False, 5) =\n",weatherRDD.takeSample(False, 5),"\n")
+    # Convert that date/time string to a datetime object
+    weatherRDD = weatherRDD.mapValues(lambda x: (x[0], x[1], datetime.strptime(x[2], "%Y-%m-%d %H:%M:%S"), datetime.strptime(x[3], "%Y-%m-%d %H:%M:%S"), x[4]) )
 
-# Join the two RDDs together?
-departureWeatherRDD = departureRDD.join(weatherRDD,N).cache()
+    # Cache it
+    weatherRDD = weatherRDD.cache()
 
-# Debug print out a few of them
-print("departureWeatherRDD.count() =",departureWeatherRDD.count(),"\n")
-print("departureWeatherRDD.takeSample(False, 5) =\n",departureWeatherRDD.takeSample(False, 5),"\n")
+    # Debug print out a few of them
+    print("weatherRDD.count() =",weatherRDD.count(),"\n")
+    print("weatherRDD.takeSample(False, 5) =\n",weatherRDD.takeSample(False, 5),"\n")
+
+    # Join the two RDDs together
+    departureWeatherRDD = departureRDD.join(weatherRDD,args.N)
+
+    # Filter down the RDD to entries where scheduled departure time (x[1][0][0]) is between weather start time (x[1][1][2]) and weather stop time (x[1][1][3])
+    departureWeatherRDD = departureWeatherRDD.filter(lambda x: x[1][1][2] <= x[1][0][0] and x[1][0][0] <= x[1][1][3])
+
+    # Cache it
+    departureWeatherRDD = departureWeatherRDD.cache()
+
+    # Debug print out a few of them
+    print("departureWeatherRDD.count() =",departureWeatherRDD.count(),"\n")
+    print("departureWeatherRDD.takeSample(False, 5) =\n",departureWeatherRDD.takeSample(False, 5),"\n")
