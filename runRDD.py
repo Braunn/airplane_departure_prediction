305a89e95d8b91a7436697eaec1ef6d7e7e1560a
Chris Wozny <1447143+cwozny@users.noreply.github.com>
2023-12-08 23:55:10 -0500

    wip on runRDD

--- runRDD.py
+++ runRDD.py
@@ -10,3 +10,8 @@ departureRDD = departureRDD.map(lambda x: x.split(","))
 
 print("departureRDD.count() =",departureRDD.count())
 print(departureRDD.take(5))
+
+departureRDD = departureRDD.map(lambda x: (x[4], x[1], x[5], x[13]) )
+
+print("departureRDD.count() =",departureRDD.count())
+print(departureRDD.take(5))
