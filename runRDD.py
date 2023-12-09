4e605338dff6e73c3798435c521175155474da1e
Chris Wozny <1447143+cwozny@users.noreply.github.com>
2023-12-09 01:36:43 -0500

    join departure and weather RDDs. too big for my laptop

--- runRDD.py
+++ runRDD.py
@@ -15,6 +15,9 @@ departureRDD = departureRDD.mapValues(lambda x: (x[0] + " " + x[1] + ":01", x[2]
 # Convert that date/time string to a datetime object
 departureRDD = departureRDD.mapValues(lambda x: (datetime.strptime(x[0], "%m/%d/%Y %H:%M:%S"), x[1]) )
 
+# Cache it
+departureRDD = departureRDD.cache()
+
 # Debug print out a few of them
 print("departureRDD.count() =",departureRDD.count(),"\n")
 print("departureRDD.takeSample(False, 5) =\n",departureRDD.takeSample(False, 5),"\n")
@@ -28,9 +31,16 @@ weatherRDD = weatherRDD.map(lambda x: (x[7],(x[1],x[2],x[3],x[4],float(x[5]))))
 # Convert that date/time string to a datetime object
 weatherRDD = weatherRDD.mapValues(lambda x: (x[0], x[1], datetime.strptime(x[2], "%Y-%m-%d %H:%M:%S"), datetime.strptime(x[3], "%Y-%m-%d %H:%M:%S"), x[4]) )
 
+# Cache it
+weatherRDD = weatherRDD.cache()
+
 # Debug print out a few of them
 print("weatherRDD.count() =",weatherRDD.count(),"\n")
 print("weatherRDD.takeSample(False, 5) =\n",weatherRDD.takeSample(False, 5),"\n")
 
+# Join the two RDDs together?
+departureWeatherRDD = departureRDD.join(weatherRDD)
 
-
+# Debug print out a few of them
+print("departureWeatherRDD.count() =",departureWeatherRDD.count(),"\n")
+print("departureWeatherRDD.takeSample(False, 5) =\n",departureWeatherRDD.takeSample(False, 5),"\n")
