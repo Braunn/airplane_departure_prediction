2fa6100652100fab61a61f63d00781c4dd99be66
Braunn <nathanaugenbraun@gmail.com>
2023-12-11 21:28:33 -0500

    added validation bash script

---
+++ run_validation.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+lscpu > cpu_info_validation_run.txt
+
+# LASSO REGRESSION MODEL
+for lam in 0.01 0.1 1 100 1000 10000;
+do spark-submit validateModel.py --master local[30] --N 30\
+                              --regParam $lam\
+                              --data_set data/processed/data_set\
+                              --K 4 --type LASSO_REG --seed 5 --intercept 1;
+done
+
+# RIDGE REGRESSION MODEL
+for lam in 0.01 0.1 1 100 1000 10000;
+do spark-submit validateModel.py --master local[30] --N 30\
+                              --regParam $lam\
+                              --data_set data/processed/data_set\
+                              --K 4 --type RIDGE_REG --seed 5 --intercept 1;
+done
+
+# LINEAR REGRESSION MODEL
+spark-submit validateModel.py --master local[30] --N 30\
+                              --regParam 0\
+                              --data_set data/processed/data_set\
+                              --K 4 --type LIN_REG --seed 5 --intercept 1
+
+spark-submit validateModel.py --master local[30] --N 30\
+                              --regParam 0\
+                              --data_set data/processed/data_set\
+                              --K 4 --type LIN_REG --seed 5 --intercept 0
